a4a399b37867d22c00d5ada7c70c73dedd2e1ee9
Max Albrecht <max.albrecht@zhdk.ch>
2018-11-30 13:43:07 +0100

    script: check for correct env

--- scripts/config-postgres-for-vagrant.sh
+++ scripts/config-postgres-for-vagrant.sh
@@ -1,7 +1,16 @@
-# run with `vagrant ssh -- sudo sh < ././scripts/config-postgres-for-vagrant.sh`
+#!/bin/sh
 
-# from https://github.com/jackdb/pg-app-dev-vm/blob/f31fc94556bfdcc6aab97f1918fd95fdc6064a8a/Vagrant-setup/bootstrap.sh#L72-L82
+# check for vagrant
+{ test $(id -u) = 0 && cd /vagrant ;} 2> /dev/null || {
+  echo ''
+  echo 'ERROR: this script must be run as root inside guest machine!'
+  echo 'try this: vagrant ssh -- 'sudo sh /vagrant/scripts/config-postgres-for-vagrant.sh''
+  echo ''
+  exit 1
+} && cd -
 
+# NOTE: script copied from postgres project, see:
+#       https://github.com/jackdb/pg-app-dev-vm/blob/f31fc94556bfdcc6aab97f1918fd95fdc6064a8a/Vagrant-setup/bootstrap.sh#L72-L82
 export PG_VERSION=10
 export PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
 export PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
