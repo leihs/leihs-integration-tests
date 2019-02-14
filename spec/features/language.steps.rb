253ae8c2f5c79862957fbce5f0da8c509cdc04b4
Matus Kmit <matus.kmit@zhdk.ch>
2019-02-14 13:40:50 +0100

    fix language feature

--- spec/features/language.steps.rb
+++ spec/features/language.steps.rb
@@ -50,7 +50,7 @@ step "the language was changed to :lang in :subapp" do |lang, subapp|
   when '/admin/', '/procure', '/my'
     find('.fa-globe').click
     find('.navbar-leihs .dropdown-menu')
-      .find('button.disabled', text: lang)
+      .find('button b', text: lang)
   when '/manage', '/borrow'
     find('footer strong', text: lang)
   else
@@ -72,7 +72,7 @@ step "the language was changed to :lang everywhere" do |lang|
     when '/admin/', '/procure', '/my'
       find('.fa-globe').click
       find('.navbar-leihs .dropdown-menu')
-        .find('button.disabled', text: lang)
+        .find('button b', text: lang)
     when '/manage', '/borrow'
       find('footer strong', text: lang)
     else
@@ -85,5 +85,5 @@ step "the current language is :lang" do |lang|
   visit "/my/user/me"
   find('.fa-globe').click
   find('.navbar-leihs .dropdown-menu')
-    .find('button.disabled', text: lang)
+    .find('button b', text: lang)
 end
