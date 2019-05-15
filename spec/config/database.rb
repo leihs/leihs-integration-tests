c32617fa167c151d8e52579a43e319d845020595
Matus Kmit <matus.kmit@zhdk.ch>
2019-05-15 16:54:00 +0200

    fix: smtp default from address

--- spec/config/database.rb
+++ spec/config/database.rb
@@ -55,7 +55,11 @@ end
 def set_settings
   fail unless LEIHS_HTTP_BASE_URL.present?
   Setting.first || Setting.create # ensure existance!
-  database.run "UPDATE settings SET external_base_url='#{LEIHS_HTTP_BASE_URL}'"
+  database.run <<-SQL
+    UPDATE settings
+    SET external_base_url = '#{LEIHS_HTTP_BASE_URL}',
+        smtp_default_from_address = 'noreply@example.com'
+  SQL
 end
 
 def resurrect_general_building
