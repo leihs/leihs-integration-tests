05c5a66570aaba8fd252e14b10a7d8b15a7fba5e
Matus Kmit <matus.kmit@zhdk.ch>
2020-04-08 14:29:00 +0200

    fix pool factory irt workdays

--- spec/features/shared/factories.steps.rb
+++ spec/features/shared/factories.steps.rb
@@ -77,7 +77,6 @@ step "the user is inventory manager of pool :name" do |name|
   pool = InventoryPool.find(name: name) ||
     FactoryBot.create(:inventory_pool, name: name)
 
-  FactoryBot.create(:workday, inventory_pool_id: pool.id)
   FactoryBot.create(:access_right,
                     user_id: @user.id,
                     inventory_pool_id: pool.id,
