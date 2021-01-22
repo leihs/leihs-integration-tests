afbdfb5f7e7bec8b142775d79dd00d63600fa4ed
Matus Kmit <matus.kmit@zhdk.ch>
2021-01-22 12:01:51 +0100

    configure wait until -> fix step

--- spec/config/helpers.rb
+++ spec/config/helpers.rb
@@ -1,8 +1,8 @@
 # test helpers
-def wait_until(wait_time = 10, &block)
+def wait_until(wait_time = 10, sleep_secs: 0.2, &block)
   Timeout.timeout(wait_time) do
     until value = yield
-      sleep(0.2)
+      sleep(sleep_secs)
     end
     value
   end
