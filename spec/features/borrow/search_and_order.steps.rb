0493d40aafa2a1274c6654c10939e311a16444a1
Max Albrecht <max.albrecht@zhdk.ch>
2021-07-13 20:39:49 +0200

    specs: adjust for new borrow

--- spec/features/borrow/search_and_order.steps.rb
+++ spec/features/borrow/search_and_order.steps.rb
@@ -33,11 +33,11 @@ step 'the show page of the model :name was loaded' do |name|
 end
 
 step 'the start date chosen previously is pre-filled in the calendar' do
-  expect(find(".rdrDateDisplay .rdrDateInput:first-child input").value).to eq Date.today.strftime('%b %-d, %Y')
+  expect(find(".ui-booking-calendar .date-range-picker input[name='startDate']").value).to eq Date.today.strftime('%d.%m.%Y')
 end
 
 step 'the end date chosen previously is pre-filled in the calendar' do
-  expect(find(".rdrDateDisplay .rdrDateInput:last-child input").value).to eq Date.tomorrow.strftime('%b %-d, %Y')
+  expect(find(".ui-booking-calendar .date-range-picker input[name='endDate']").value).to eq Date.tomorrow.strftime('%d.%m.%Y')
 end
 
 step 'the start date chosen previously is pre-filled in the search panel' do
