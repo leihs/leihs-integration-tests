ba221d4bf04bdd7d58c4b3534569303a9a86e567
Matus Kmit <matuskmit1@gmail.com>
2024-03-26 14:53:49 +0100

    chore: create mail templates for new pool trigger

--- spec/factories/inventory_pool.rb
+++ spec/factories/inventory_pool.rb
@@ -1,8 +1,5 @@
 class InventoryPool < Sequel::Model
-  def after_create
-    FactoryBot.create(:workday, inventory_pool_id: self.id)
-    super
-  end
+  one_to_one(:workday)
 end
 
 FactoryBot.define do
@@ -22,16 +19,8 @@ FactoryBot.define do
       ip.shortname = short
     end
 
-    after :create do |ip|
-      MailTemplate.where(is_template_template: true).each do |meta_templ|
-        attrs = 
-          meta_templ
-          .to_hash
-          .merge(inventory_pool_id: ip.id, is_template_template: false)
-        attrs.delete(:id)
-
-        create(:mail_template, attrs)
-      end
+    after(:create) do |ip|
+      ip.workday.update(saturday: true, sunday: true)
     end
   end
 end
