01afe55c5c2567ef25a777beed0d78f641631142
Matus Kmit <matuskmit1@gmail.com>
2025-03-11 14:18:45 +0100

    fix: release info steps 2

--- spec/features/release_info.steps.rb
+++ spec/features/release_info.steps.rb
@@ -1,7 +1,4 @@
 step 'I see the correct release version in the footer' do
-  release_file_path = File.join('../LEIHS-VERSION')
-  content = File.read(release_file_path)
-  version = content.split("\n").first
   gh_url = 'https://github.com/leihs/leihs'
-  find("footer a[href='#{gh_url}']", text: version)
+  find("footer a[href='#{gh_url}']", text: 'x.y.z-beta')
 end
